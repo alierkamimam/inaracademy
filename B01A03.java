c0645aaef54a6a892a48ea0b98db78951842fba1
ALİ ERKAM İMAM <111283289+alierkamimam@users.noreply.github.com>
2022-08-20 14:49:22 +0300

    Update B01A03.java

--- B01A03.java
+++ B01A03.java
@@ -5,7 +5,7 @@ public class B01A03 {
 		System.out.println("    J	    A	    V	  V	  A "); 
 		System.out.println("    J	   A A 	     V   V	 A A");		
 		System.out.println(" J  J	  AAAAA       V V       AAAAA");
-		System.out.println("  JJ     A     A       V       A     A ");		
+		System.out.println("  JJ         A     A       V       A     A ");		
 				
 							
 	}
