cf4e4dc8b610391e612e9808c412db614ba804d6
ALİ ERKAM İMAM <111283289+alierkamimam@users.noreply.github.com>
2022-08-20 14:48:59 +0300

    Update B01A04.java

--- B01A04.java
+++ B01A04.java
@@ -3,10 +3,10 @@ package bolum01.alistirma;
 public class B01A04 {
    public static void main( String[] args) {
 	  System.out.println(" a         a^2      a^3\n"  +
-			             " 1         1        1 \n"   +
-			  			 " 2         4        8 \n"  +
-			             " 3         9        27\n"   +
-			  			 " 4        16        64\n");     
+			     " 1         1        1 \n"   +
+	                     " 2         4        8 \n"  +
+			     " 3         9        27\n"   +
+			     " 4        16        64\n");     
 	
    }
 }
